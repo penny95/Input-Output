5eae5a04866463382a40442b482ac94ee9defbc5
penny95 <49157799+penny95@users.noreply.github.com>
2019-12-16 14:08:22 +0300

    added process.stdin.once

--- myscript.js
+++ myscript.js
@@ -1,20 +1,21 @@
+console.log("Enter year of birth");
+process.stdin.once('data', (yearOfBirth)=>{
+  var yearOfBirth = Number(yearOfBirth);
 function validateAge(){
     var currentDate = new Date();
     const currentYear = currentDate.getFullYear();
-  
-    var yearOfBirth = prompt("Enter year of birth");
-    var age=currentYear - yearOfBirth;
+    var age=currentYear- yearOfBirth;
+   
     if(age<18){
-      console.log("minor");
+     console.log("minor");
      
      } 
      else if ( age >=18 && age <=36){
-      console.log("youth");
-     
+    console.log("youth");   
      } 
     else {
-      console.log("adult");
+      console.log("adults");
     }
-    
-}
-validateAge();
+  }
+  validateAge();
+})
